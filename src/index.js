3aaf833b22dac329eeb931a76ddd1134f94ca3f0
Jenna <{ID}+{username}@users.noreply.github.com>
2024-03-13 15:07:05 -0500

    Update Practice for axios.get
    axios.get(getUserName)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+
+let apiUrl = "https://jsonplaceholder.typicode.com/users";
+
+
 function displayUsername(response) {
 	console.log(response.data.username);
 
@@ -5,7 +9,32 @@ function displayUsername(response) {
 	h1.innerHTML = `Hey ${response.data.username}`;
 }
 
+let userId = 1; // Assign a value to the userId variable
+let getUserName = `${apiUrl}/${userId}`;
 
-let apiUrl = "https://jsonplaceholder.typicode.com/users/1";
+axios.get(getUserName).then(displayUsername);
 
-axios.get(apiUrl).then(displayUsername);
+//const element = document.querySelector('.your-class');
+//In this example, the variable element
+//will store the first element having the class name your - class.
+
+
+// querySelector("#myElement"); 
+// In the above example, the querySelector method is used to 
+// select the element with the ID "myElement".The ID is specified 
+// as "#myElement" in the CSS selector format.
+// This will return the first element that matches the specified ID.
+
+
+function displayFullName(response) {
+	console.log(response.data.name);
+
+	let fullName = document.querySelector('.fullName');
+	fullName.innerHTML = `Hello, my name is ${response.data.name}!`;
+}
+
+let userId2 = 5;
+let getName = `${apiUrl}/${userId2}`;
+
+axios.get(getName).then(displayFullName);
+
